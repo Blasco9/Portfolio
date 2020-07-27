8a158d0b789df2f62220b8e4bc7b8285c0a142dc
Julian Blasco <blascoblasco9@gmail.com>
2020-07-26 22:42:05 -0300

    Add project animations on scroll

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const intro = document.querySelector('.intro');
 const introName = document.querySelector('.intro-name');
 const introDescription = document.querySelector('.intro-description');
 const introLink = document.querySelector('.intro-link');
+const project1 = document.querySelector('.project-card');
+const project2 = document.querySelector('.project-card:nth-of-type(2)');
+const project3 = document.querySelector('.project-card:nth-of-type(3)');
 
 window.onscroll = function() {
   if(window.scrollY > (intro.scrollHeight - nav.offsetHeight)) {
@@ -29,5 +32,29 @@ window.onscroll = function() {
   } else {
     introLink.style.color = 'inherit';
     introLink.style.borderColor = 'inherit';
+  }
+
+  if((window.scrollY + nav.offsetHeight) > project1.offsetTop) {
+    project1.classList.remove('slide-right')
+    project1.classList.add('slide-left')
+  } else {
+    project1.classList.remove('slide-left')
+    project1.classList.add('slide-right')
+  }
+
+  if((window.scrollY + nav.offsetHeight) > project2.offsetTop) {
+    project2.classList.remove('slide-right')
+    project2.classList.add('slide-left')
+  } else {
+    project2.classList.remove('slide-left')
+    project2.classList.add('slide-right')
+  }
+
+  if((window.scrollY + nav.offsetHeight) > project3.offsetTop) {
+    project3.classList.remove('slide-right')
+    project3.classList.add('slide-left')
+  } else {
+    project3.classList.remove('slide-left')
+    project3.classList.add('slide-right')
   }
 }
